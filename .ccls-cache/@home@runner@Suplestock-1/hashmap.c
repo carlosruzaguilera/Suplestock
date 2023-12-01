8a442ac0e9e7565c2731f582a416dffe8d346f02
carlosruzaguilera <142541087+carlosruzaguilera@users.noreply.github.com>
2023-12-01 23:00:27 +0000

    eliminar producto por nombre
    se programo la lógica de la función eliminar por nombre

--- .ccls-cache/@home@runner@Suplestock-1/hashmap.c
+++ .ccls-cache/@home@runner@Suplestock-1/hashmap.c
@@ -68,3 +68,42 @@ void eliminarProducto() {
 
     printf("\nProducto con ID %d no encontrado.\n", id);
 }
+
+void eliminarProductoPorNombre() {
+    char nombreBuscado[50];
+    printf("\nIngrese el nombre del producto a eliminar: ");
+    scanf("%s", nombreBuscado);
+
+    int encontrado = 0;
+
+    for (int i = 0; i < TAM_TABLA; ++i) {
+        Producto *actual = tablaHash[i];
+        Producto *anterior = NULL;
+
+        while (actual != NULL) {
+            if (strcmp(actual->nombre, nombreBuscado) == 0) {
+                encontrado = 1;
+                if (anterior == NULL) {
+                    // Eliminar el primer producto en la lista
+                    tablaHash[i] = actual->siguiente;
+                } else {
+                    // Eliminar un producto que no es el primero
+                    anterior->siguiente = actual->siguiente;
+                }
+                free(actual);
+                printf("\nProducto con nombre '%s' eliminado.\n", nombreBuscado);
+                break; // Salir del bucle si se encuentra y elimina el producto
+            }
+            anterior = actual;
+            actual = actual->siguiente;
+        }
+
+        if (encontrado) {
+            break; // Salir del bucle principal si se ha eliminado el producto
+        }
+    }
+
+    if (!encontrado) {
+        printf("\nProducto con nombre '%s' no encontrado.\n", nombreBuscado);
+    }
+}
