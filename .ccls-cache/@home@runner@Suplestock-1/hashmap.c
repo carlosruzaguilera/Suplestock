e39308695de5e5c1d45eb63caac1aab83a06d01a
carlosruzaguilera <142541087+carlosruzaguilera@users.noreply.github.com>
2023-12-01 22:56:28 +0000

    funciones de gestión y registrar
    se definieron cuales son las funciones de gestión de productos y se programo la función registrar producto

--- .ccls-cache/@home@runner@Suplestock-1/hashmap.c
+++ .ccls-cache/@home@runner@Suplestock-1/hashmap.c
@@ -7,3 +7,34 @@ Venta *tablaHashVentas[TAM_TABLA_VENTAS] = {NULL}; // Inicializar todas las entr
 int calcularHash(int id) {
     return id % TAM_TABLA;
 }
+
+void registrarProducto() {
+    Producto producto;
+
+    printf("\nIngrese ID del producto: ");
+    scanf("%d", &producto.id);
+    printf("\nIngrese nombre del producto: ");
+    scanf("%s", producto.nombre);
+    printf("\nIngrese precio de compra: ");
+    scanf("%f", &producto.precio_compra);
+    printf("\nIngrese precio de venta: ");
+    scanf("%f", &producto.precio_venta);
+    printf("\nIngrese cantidad en stock: ");
+    scanf("%d", &producto.cantidad_stock);
+
+    int indice = calcularHash(producto.id);
+
+    Producto *nuevoProducto = (Producto *)malloc(sizeof(Producto));
+    *nuevoProducto = producto;
+    nuevoProducto->siguiente = NULL;
+
+    if (tablaHash[indice] == NULL) {
+        tablaHash[indice] = nuevoProducto;
+    } else {
+        Producto *actual = tablaHash[indice];
+        while (actual->siguiente != NULL) {
+            actual = actual->siguiente;
+        }
+        actual->siguiente = nuevoProducto;
+    }
+}
